86bc2af2da1306bfeee7da9288e708f0657920ca
Laura del Toro <laura.deltoro@seddi.com>
2022-06-07 14:20:00 +0100

    Add alert when number is invalid

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,8 +1,36 @@
-import { TextInput, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
+  const [inputNumber, setInputNumber] = useState("");
+
+  const numberInputHandler = (text) => {
+    setInputNumber(text);
+  };
+  const resetInputHandler = () => setInputNumber("");
+
+  const confirmInputHandler = () => {
+    const number = parseInt(inputNumber);
+    if (isNaN(number) || number <= 0 || number > 99) {
+      Alert.alert(
+        "Invalid number",
+        "Number has to be a number between 1 and 99",
+        [
+          {
+            text: "Ok",
+            style: "destructive",
+            onPress: resetInputHandler,
+          },
+        ]
+      );
+
+      return;
+    }
+    console.log("VALID NUMBER");
+  };
+
   return (
     <View style={styles.inputContainer}>
       <TextInput
@@ -11,13 +39,15 @@ const StartGameScreen = () => {
         keyboardType="number-pad"
         autoCapitalize="none"
         autoCorrect={false}
+        onChangeText={numberInputHandler}
+        value={inputNumber}
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonsContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonsContainer}>
-          <PrimaryButton>Confirm</PrimaryButton>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
         </View>
       </View>
     </View>
